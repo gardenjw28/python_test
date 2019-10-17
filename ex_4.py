a70e22eb4441ebb63a66d0faa4578879fdf1febf
gardenjw28 <55496392+gardenjw28@users.noreply.github.com>
2019-10-17 23:23:51 +0900

    Update ex_4.py

--- ex_4.py
+++ ex_4.py
@@ -2,9 +2,13 @@
 #문자열에서 쉼표 뒤 입력된 문자열의 2번째 index 값을 출력하는 것
 
 def second_index(text: str, symbol: str):
+    
+    #만약 symbol의 갯수가 2개 미만이면 2번째 index값이 없으니까 None으로 출력
     if text.count(symbol) < 2:
         return 'None'
-
+    
+    #text에서 symbol을 찾아서 그 index값을 first에 할당
     first = text.find(symbol)
-
+    
+    #text에서 symbol을 찾는데 first에 +1을 해서 여기서부터 symbol의 index값을 찾아서 return
     return print("두번째 index값:", text.find(symbol, first + 1))
